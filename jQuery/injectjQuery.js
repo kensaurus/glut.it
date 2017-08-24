2a0e83f48d935bb71b15b14149232fc4dbb5ef26
kenji <kensaurus@gmail.com>
2017-08-24 14:14:16 +0800

    Update and rename Inject.js to injectjQuery.js

--- jQuery/injectjQuery.js
+++ jQuery/injectjQuery.js
@@ -1,4 +1,6 @@
-//Inject jQuery
+//@kenjisakuramoto
+//?Inject jQuery
+
 var jq = document.createElement('script');
 jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js";
 document.getElementsByTagName('head')[0].appendChild(jq);
