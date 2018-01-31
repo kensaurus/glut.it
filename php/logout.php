b4d11f4bd2af48581d7c33d78ba819a98e862705
kenji <kensaurus@gmail.com>
2018-01-31 15:39:46 +0800

    Create logout.php

---
+++ php/logout.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+session_destroy();
+header('Location: index.php');
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<?php require_once "bootstrap.php"; ?>
+<title>Kenji Sakuramoto</title>
+</head>
+<body>
+</body>
+
