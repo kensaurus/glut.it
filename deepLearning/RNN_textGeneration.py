31cde6e8a8fccb5b50762e0217fd94b2240e3e09
kenji <kensaurus@gmail.com>
2018-02-10 15:27:54 +0800

    Create RNN_textGeneration.py

---
+++ deepLearning/RNN_textGeneration.py
@@ -0,0 +1,14 @@
+import numpy as np
+from utils import *
+import random
+
+data = open('dinos.txt', 'r').read()
+data= data.lower()
+chars = list(set(data))
+data_size, vocab_size = len(data), len(chars)
+print('There are %d total characters and %d unique characters in your data.' % (data_size, vocab_size))
+
+char_to_ix = { ch:i for i,ch in enumerate(sorted(chars)) }
+ix_to_char = { i:ch for i,ch in enumerate(sorted(chars)) }
+print(char_to_ix)
+print(ix_to_char)
