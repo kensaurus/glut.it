8eebd7af189c51993e6f0ba4fd688953aae5a7ef
kenji <kensaurus@gmail.com>
2018-02-14 17:02:23 +0800

    Create RNN_workVectors.py

---
+++ deepLearning/RNN_workVectors.py
@@ -0,0 +1,34 @@
+import numpy as np
+from w2v_utils import *
+
+# Pre-trained Glove vectors
+words, word_to_vec_map = read_glove_vecs('data/glove.6B.50d.txt')
+
+def cosine_similarity(u, v):
+    """
+    Cosine similarity reflects the degree of similariy between u and v
+        
+    Arguments:
+        u -- a word vector of shape (n,)          
+        v -- a word vector of shape (n,)
+
+    Returns:
+        cosine_similarity -- the cosine similarity between u and v defined by the formula above.
+    """
+    
+    distance = 0.0
+    
+    # Compute the dot product between u and v (≈1 line)
+    dot = np.dot(u,v)
+    # Compute the L2 norm of u (≈1 line)
+    norm_u = np.linalg.norm(u)
+    
+    # Compute the L2 norm of v (≈1 line)
+    norm_v = np.linalg.norm(v)
+    # Compute the cosine similarity defined by formula (1) (≈1 line)
+    cosine_similarity = dot/(norm_u*norm_v)
+    
+    return cosine_similarity
+    
+    
+    
