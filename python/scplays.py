9485665c7e8e359afc6d9c4e09e10fbc7bc4634f
kenji <kensaurus@gmail.com>
2018-02-15 19:50:09 +0800

    Update scplays.py

--- python/scplays.py
+++ python/scplays.py
@@ -1,17 +1,13 @@
 '''
-kenjisakuramoto
+kenji-sakuramoto
 scrape soundcloud & mixcloud plays
-print out the leftover plays needed for the order
-place "scplays.csv" in the same folder
-1st column HTML, 2nd column current plays, 3rd column expected plays
 '''
 
 #load modules
+import time
+import os
 from bs4 import BeautifulSoup
-from pandas import *
-#import scrapy
-#import csv
-#from datetime import datetime
+import pandas as pd
 try:
     # Python 3.0
     from urllib.request import urlopen
@@ -20,42 +16,64 @@ except ImportError:
     from urllib2 import urlopen
 
 #load excel 
-''' 
-with open('scplays.csv', 'r') as f:
-    reader = csv.reader(f)
-    sc_page = list(reader)
-'''
-sc_data = read_csv("scplays.csv")
-sc_page = sc_data.ix[:,0]
-sc_expected = sc_data.ix[:,3]
-
-#list comprehension
-#sc_page = [l[0] for l in sc_page]
+df = pd.read_csv(os.path.join(os.path.dirname(__file__),"scplays.csv"), encoding="ISO-8859-1")
+sc_url = df.ix[:,0]
+sc_start = df.ix[:,1].fillna(-1)
+sc_bought = df.ix[:,2]
+sc_expected = df.ix[:,3].fillna(-1)
+sc_last = df.ix[:,4].fillna(0)
 
 #query & parse
-data = []
-x = 0
-for page in sc_page:
-    sc_page_query = urlopen(page)
-    sc_page_parse = BeautifulSoup(sc_page_query, 'html.parser')
+print(time.ctime())
 
-#find
-    track = sc_page_parse.find('meta', property="og:title", content=True)
-    plays = sc_page_parse.find('meta', property="soundcloud:play_count", content=True)
-    data.append((track, plays))
-    print(track["content"] if track else "No meta title given")
-    print(plays["content"] if plays else "No meta title given")
-    #sc_expected[x] = int(sc_expected[x]) - int(plays["content"])
-    print(sc_expected[x])
-    print('-------------------------------------------')
+#loop through each url
+x=0
+for url in sc_url:
+    try:
+        sc_page_query = urlopen(url)
+    except:
+        print("Error", url)
+        x += 1
+        continue
+    
+    sc_page_parse = BeautifulSoup(sc_page_query, 'html.parser')
+    
+    #find in page
+    if "soundcloud" in url:
+        try:
+            track = sc_page_parse.find('meta', property="og:title", content=True)
+            plays = sc_page_parse.find('meta', property="soundcloud:play_count", content=True)
+            plays = int(plays["content"])
+        except:
+            track = "Error"
+            plays = "-1"
+        #check if start or expected plays is empty
+        if sc_start[x] == -1:
+            sc_start[x] = plays
+        if sc_expected[x] == -1:
+            sc_expected[x] = sc_start[x]+sc_bought[x]
+        print(url.replace("https://",""))
+        print("Left: ",sc_expected[x]-plays,"\tSC: ",plays-sc_last[x])
+        sc_last[x] = plays
+    if "mixcloud" in url:
+        try:
+            track = sc_page_parse.find('meta', property="og:title", content=True)
+            plays = int(sc_page_parse.find('li', {'data-tooltip':True}).text.strip().replace(',',''))
+        except:
+            track = "Error"
+            plays = "-1"
+        #check if start or expected plays is empty
+        if sc_start[x] == -1:
+            sc_start[x] = plays
+        if sc_expected[x] == -1:
+            sc_expected[x] = sc_start[x]+sc_bought[x]
+        print(url.replace("https://www.",""))
+        print("Left: ",sc_expected[x]-plays,"\tMC: ",plays-sc_last[x])
+        sc_last[x] = plays
     x += 1
 
-'''
-#save file
-with open('plays.csv', 'a') as csv_file:
-    writeplays = csv.writer(csv_file)
-    for track, plays in data:
-        if plays is None:
-            continue
-        writeplays.writerow([datetime.now(), track["content"], plays["content"]])
-'''
+#write to file
+df['start'] = pd.Series(sc_start, index=df.index[:len(sc_start)])
+df['expected'] = pd.Series(sc_expected, index=df.index[:len(sc_expected)])
+df['last'] = pd.Series(sc_last, index=df.index[:len(sc_last)])
+df.to_csv(os.path.join(os.path.dirname(__file__),"scplays.csv"), encoding="ISO-8859-1", index=False)
